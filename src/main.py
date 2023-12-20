6c41201f32aa75bd2dc574ff815086141df561b7
Anand <anandshende1994@gmail.com>
2023-12-21 05:01:14 +0530

    spliting candidate summary and finding relevant question by semantic match

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ from utils.vectordb_operations import (
 
 if __name__ == "__main__":
     df = pd.read_excel('../data/processed/combined_dataset.xlsx')
+    
     # Run the below command only once. It creates the ChromaDB collection in the data/chromadb_dir directory.
     # generate_qa_vector_db(
     #     os.environ.get('CHROMADB_PATH', '../data/chromadb'),
@@ -17,6 +18,6 @@ if __name__ == "__main__":
     # )
     
     generate_questions(
-        'Medical Assistant',
-        ''
+        'Nurse',
+        'Dedicated and compassionate Registered Nurse with a diverse background in healthcare. Holds a [Degree or Certification] in Nursing from [Institution]. Proven expertise in providing patient-centered care, managing medical records, and collaborating with interdisciplinary teams. Skilled in administering medications, monitoring vital signs, and implementing nursing care plans. Demonstrates strong communication and interpersonal skills, fostering positive relationships with patients, families, and healthcare professionals. Upholds a commitment to continuous learning and professional development. Adept at maintaining a calm and focused demeanor in high-pressure situations. Excited about contributing clinical skills and compassionate care to a dynamic healthcare environment. [Optional: Specify any specializations, such as critical care, pediatrics, or other relevant areas of expertise.]'
     )
--- src/utils/generate_questions.py
+++ src/utils/generate_questions.py
@@ -2,8 +2,8 @@ import os
 import pandas as pd
 import chromadb
 
+from utils.common import split_into_sentences
 from utils.vectordb_operations import get_collection_from_vector_db
-
 
 def generate_questions(
     position: str, candidate_profile: str
@@ -24,11 +24,29 @@ def generate_questions(
     vdb = os.environ.get('CHROMADB_PATH', '../data/chromadb')
     question_collection: chromadb.Collection = get_collection_from_vector_db(vdb, 'question_collection')
     
-    query_response = question_collection.query(
-        query_texts=[candidate_profile],
-        where={'position': position}
-    )
+    sentences = split_into_sentences(candidate_profile)
+    question_df = pd.DataFrame(columns=["question", "q_id", "interview_phase", "position"])
     
-    print(query_response)
+    for sentence in sentences:
+        query_response = question_collection.query(
+            query_texts=[sentence],
+            where={'position': position}
+        )
     
-    return pd.DataFrame()
+        for q_id, metadata, document in zip(query_response['ids'][0], query_response['metadatas'][0], query_response['documents'][0]):
+            row = pd.DataFrame(data={
+                'q_id': [q_id],
+                'interview_phase': [metadata.get('interview_phase', None)],
+                'position': [metadata.get('position', None)],
+                'question': [document]
+            })
+            
+            question_df = pd.concat([
+                question_df,
+                row
+            ])
+        
+    question_df = question_df.drop_duplicates(subset=['question']).reset_index(drop=True)
+    print(question_df)
+    
+    return question_df
